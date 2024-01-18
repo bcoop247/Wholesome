38e666cf306b76ad6eaef37d3eecdbbda0937ed8
bcoop247 <brcooper247@gmail.com>
2024-01-17 22:46:29 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Login from './Login';
 import NewUser from './NewUser';
 import Home from './Home';
 import Meals from './Meals';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {  HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Router basename = '/Wholesome'>
+    <Router basename = '/'>
         <Routes>
             <Route index element = {<Login />} />
             <Route path = '/newuser' element = {<NewUser />} />
             <Route path = '/home' element = {<Home />} />
             <Route path = '/meals' element = {<Meals />} />
         </Routes>
+
     </Router>
    
     </>
